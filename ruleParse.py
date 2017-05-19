9c99a3913302025b03fda1bd6217ea54f7f00aae
liaozy <798389647@qq.com>
2017-05-20 00:30:03 +0800

    modify static method to private func

--- ruleParse.py
+++ ruleParse.py
@@ -3,45 +3,40 @@
 import os
 class Signature(object):
     def __init__(self,line):
-        self.id  = Signature._getsid_(line)
-        self.version = Signature._getversion_(line)
+        self.id  = _getsid_(line)
+        self.version = _getversion_(line)
         self.rule = line
 
-    @staticmethod
-    def _isvalid_(line):
-        if len(line) >= 5 and line[0:5] == 'alert':
-            return True
-        return False
+def _isvalid_(line):
+    if len(line) >= 5 and line[0:5] == 'alert':
+        return True
+    return False
 
-    @staticmethod
-    def _getsid_(line):
-        sid = -1
-        if Signature._isvalid_(line):
-            idx = line.find("; sid:")
-            if idx != -1:
-                sid = line[idx+6: idx+13]
-        return sid
+def _getversion_(line):
+    return 0 
 
-    @staticmethod
-    def _getversion_(line):
-        return 0 
+def _getsid_(line):
+    sid = -1
+    if _isvalid_(line):
+        idx = line.find("; sid:")
+        if idx != -1:
+            sid = line[idx+6: idx+13]
+    return sid
 
 class RuleFile(object):
     def __init__(self,filePath):
         self.fileName = filePath
-        self.signatures = RuleFile._parse_(filePath)
+        self.signatures = _parse_(filePath)
 
-    @staticmethod
-    def _parse_(filePath):
-        with open(filePath) as f:
-            signature = []
-            for line in f.readlines():
-                sig = Signature(line)
-                if sig.id == -1:
-                    continue
-                signature.append(sig)
-            return signature
-
+def _parse_(filePath):
+    with open(filePath) as f:
+        signature = []
+        for line in f.readlines():
+            sig = Signature(line)
+            if sig.id == -1:
+                continue
+            signature.append(sig)
+        return signature
 
 def _getFileList_(dirPath):
     if os.path.isdir(dirPath):
